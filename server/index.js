f1093c357f6284abad9f022eafbe5c6fa70819de
sporthq <nowak.sebastian991@gmail.com>
2023-06-20 17:49:54 +0200

    finished project #2

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+dotenv.config();
 import connectToDatabase from './database.js';
 import express from 'express';
 
@@ -9,26 +10,29 @@ import enrolledRoutes from './routes/enrolledCastingRoutes.js';
 import contactUSRoutes from './routes/contactUsRoute.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-
-
-dotenv.config();
 
 connectToDatabase();
 const app = express();
-
 app.use(express.json());
-// app.use(express.urlencoded({extended:true}))
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-const port = process.env.PORT || 5000;
-
 app.use('/api/castings', castingRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/', enrolledRoutes);
 app.use('/api', contactUSRoutes);
+const port = process.env.PORT || 5000;
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/client/build')));
+
+	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
+
 app.listen(port, () => {
 	console.log(`Server run on port ${port}`);
 });
