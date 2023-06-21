0c0a114e8cb863f06648c789d484bd9d33dfef81
sporthq <nowak.sebastian991@gmail.com>
2023-06-21 10:08:45 +0200

    test

--- server/index.js
+++ server/index.js
@@ -20,12 +20,12 @@ app.use('/api/', enrolledRoutes);
 app.use('/api', contactUSRoutes);
 const port = process.env.PORT || 5000;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// const __dirname = path.resolve();
-// app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '/client/build')));
