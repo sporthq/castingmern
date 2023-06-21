6b3e5a2026a65c0286e49a753cd9ac8d2b65896d
sporthq <nowak.sebastian991@gmail.com>
2023-06-21 10:00:29 +0200

    change settting in server

--- server/index.js
+++ server/index.js
@@ -27,11 +27,11 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 // const __dirname = path.resolve();
 // app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-// if (process.env.NODE_ENV === 'production') {
-// 	app.use(express.static(path.join(__dirname, '/client/build')));
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/client/build')));
 
-// 	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
-// }
+	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 app.listen(port, () => {
 	console.log(`Server run on port ${port}`);
